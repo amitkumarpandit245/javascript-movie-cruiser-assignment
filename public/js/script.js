485ad22ae2139504811068ced9e3f2838dde33b9
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-11-27 15:42:12 +0000

    JS1

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,43 @@
-function getMovies() {
+function getMovies(event)
+{
+    fetch('http://localhost:3000/movies')
+    .then(response=>{
+        if(response.status == 200)
+        {
+            return Promise.resolve(response.json());
+        }
+        else
+        {
+            return Promise.reject(new Error('Unable to fetch the data'));
+        }
+    }).then(movieData=>{
 
+            const tbody = document.getElementsByTagName('li')[0];
+            let stdInnerHtml='';
+            movieData.forEach(movie => {
+                stdInnerHtml = stdInnerHtml + `
+                                    <tr>
+                                        <table>
+                                        <tr>
+                                       
+                                        </tr>
+                                        <tr>
+                                            <img src="${movie.posterPath}" width="200" height="250"/>
+                                        </tr>
+                                        <tr>
+                                        <h4>${movie.title}</h4>
+                                        </tr>
+                                        <tr>
+                                            <button class="btn btn-primary">Add To Favourite</button>
+                                        </tr>
+                                        </table>
+                                    </tr>
+                                `;                
+                                tbody.innerHTML=stdInnerHtml;
+            });
+    }).catch(err=>{
+        console.log(err);
+    })
 }
 
 function getFavourites() {
