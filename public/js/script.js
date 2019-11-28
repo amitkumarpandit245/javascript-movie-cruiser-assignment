a3afd17ba1269a26a0e76d761e83ebec5cc06761
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-11-28 07:44:43 +0000

    Movie

--- public/js/script.js
+++ public/js/script.js
@@ -1,126 +1,116 @@
-function getMovies(event)
-{
-    fetch('http://localhost:3000/movies')
-    .then(response=>{
-        if(response.status == 200)
-        {
-            return Promise.resolve(response.json());
-        }
-        else
-        {
-            return Promise.reject(new Error('Unable to fetch the data'));
-        }
-    }).then(movieData=>{
+let movies = [];
+let favourites = [];
+let moviesURL = 'http://localhost:3000/movies';
+let favURL = 'http://localhost:3000/favourites';
 
-            const tbody = document.getElementById('moviesList');
-            let stdInnerHtml='';
-            movieData.forEach(movie => {
-                stdInnerHtml = stdInnerHtml + `
-                                    <tr>
-                                        <table>
-                                        <tr>
-                                       
-                                        </tr>
-                                        <tr>
-                                            <img src="${movie.posterPath}" width="200" height="250"/>
-                                        </tr>
-                                        <tr>
-                                        <h4>${movie.title}</h4>
-                                        </tr>
-                                        <tr>
-                                            <button class="btn btn-primary" onclick="addFavourite('+${movie.id}+')">Add To Favourite</button>
-                                        </tr>
-                                        </table>
-                                    </tr>
-                                `;                
-                                tbody.innerHTML=stdInnerHtml;
-            });
-    }).catch(err=>{
-        console.log(err);
-    })
+
+/* Function to return movies */
+function listMovies() {
+	let movHtml = movies.map(movie => {
+		return `
+        <tr>
+            <table>
+            <tr>
+           
+            </tr>
+            <tr>
+                <img src="${movie.posterPath}" width="200" height="250"/>
+            </tr>
+            <tr>
+            <h4>${movie.title}</h4>
+            </tr>
+            <tr>
+                <button class="btn btn-primary" onclick="addFavourite(${movie.id})">Add To Favourite</button>
+            </tr>
+            </table>
+        </tr>
+    `;   
+		});
+	let mov = document.getElementById('moviesList');
+	mov.innerHTML = movHtml;
+}
+/* Function to get list of movies */
+function getMovies() {
+	return fetch(moviesURL)
+	.then(response => response.json())
+	.then(resp => {
+		movies = resp;
+		listMovies();
+		return resp;
+	})
+	.catch((err)=> { throw new Error(err); });
 }
 
 
-function getFavourites(event) {
-    fetch('http://localhost:3000/favourites')
-    .then(response=>{
-        if(response.status == 200)
-        {
-            return Promise.resolve(response.json());
-        }
-        else
-        {
-            return Promise.reject(new Error('Unable to fetch the data'));
-        }
-    }).then(favMovieData=>{
-
-            const tbody = document.getElementById('favouritesList');
-            let stdInnerHtml='';
-            favMovieData.forEach(movie => {
-                stdInnerHtml = stdInnerHtml + `
-                                    <tr>
-                                        <table>
-                                        <tr>
-                                       
-                                        </tr>
-                                        <tr>
-                                            <img src="${movie.posterPath}" width="200" height="250"/>
-                                        </tr>
-                                        <tr>
-                                        <h4>${movie.title}</h4>
-                                        </tr>
-                                        </table>
-                                    </tr>
-                                `;                
-                                tbody.innerHTML=stdInnerHtml;
-            });
-    }).catch(err=>{
-        console.log(err);
-    })
+/* Function to return favourites */
+function listFavourites() {
+	let favHtml = favourites.map(movie => {
+		return  `
+        <tr>
+            <table>
+            <tr>
+           
+            </tr>
+            <tr>
+                <img src="${movie.posterPath}" width="200" height="250"/>
+            </tr>
+            <tr>
+            <h4>${movie.title}</h4>
+            </tr>
+            </table>
+        </tr>
+    `;     
+		});
+	let fav = document.getElementById('favouritesList');
+	fav.innerHTML = favHtml;
 }
 
+/* Function to get list of favourites */
+function getFavourites() {
+return fetch(favURL)
+	.then(response => response.json())
+	.then(resp => {
+		favourites = resp;
+		listFavourites();
+	return resp;
+})
+.catch((err)=> { throw new Error(err); });
+}
+
+
+/* Function to add favourite from ID */
 function addFavourite(id) {
-    let title;
-    let path;
-    
-    const favmovie={
-        "title":title,
-        "posterPath":path,
-        }
-
-        fetch('http://localhost:3000/movies/'+id,{
-            method:'GET',
-            headers:{
-                'content-type':'application/json'
-            }
-        }).then(response=>response.json())
-        .then(data => {
-            title=data.title;
-            path=data.posterPath;
-            var p1 = new Promise(
-                (resolve, reject) => {
-                    fetch('http://localhost:3000/favourites', {
-                        method: 'POST',
-                        headers: {
-                            'content-type': 'application/json'
-                        },
-                        body: JSON.stringify(favmovie)
-                    });
-                }
-            );
-        });
-
+let mov = movies.filter(m => {
+    return m.id === id;
+  });
+let fav = favourites.filter(f => {
+    return f.id === id;
+  });
+	 if(fav.length > 0) {
+		throw new Error('Movie is already added to favourites');
+	}
+	else {
+		return fetch(favURL, {
+			method: 'POST',
+			body: JSON.stringify(mov[0]),
+			headers: { 'Content-Type': 'application/json' }
+		}).then(function(response) {
+			return response.json();
+		}).then(function(fav1) {
+			favourites.push(fav1);
+			listFavourites();
+			return favourites;
+		});
+	}
 }
+	
 
 module.exports = {
 	getMovies,
 	getFavourites,
 	addFavourite
 };
-
 // You will get error - Uncaught ReferenceError: module is not defined
 // while running this script on browser which you shall ignore
 // as this is required for testing purposes and shall not hinder
 // it's normal execution
-
-
